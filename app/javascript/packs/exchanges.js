a10ac6d98aad37636dbc1cd9c2a4b200c158a2b9
Hanoch Griner <nokgriner@gmail.com>
2020-05-14 17:31:26 -0700

    INIT

---
+++ app/javascript/packs/exchanges.js
@@ -0,0 +1,10 @@
+result()
+
+function result() {
+  document.addEventListener('turbolinks:load', function () {
+    document.getElementById('exchange_form').addEventListener('ajax:success', function (event) {
+      let [result] = event.detail
+      document.getElementById('result').value = result.value
+    })
+  })
+}
